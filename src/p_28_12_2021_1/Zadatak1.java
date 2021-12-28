6c0d4936c949d658bb70b894e5a00ede6011baf1
Aleksandra Radojevic <aleksandra.radojevic93@gmail.com>
2021-12-28 19:47:28 +0100

    print array

--- src/p_28_12_2021_1/Zadatak1.java
+++ src/p_28_12_2021_1/Zadatak1.java
@@ -3,8 +3,21 @@ package p_28_12_2021_1;
 public class Zadatak1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[] array = {1,2,3,4,5};
+		
+		printArray(array);
+
+	}
+	
+	public static void printArray(int[] array) {
+		
+		for (int i = 0; i < array.length; i++) {
+			
+			System.out.println(array[i]);
+			
+		}
+		
 	}
 
 }
